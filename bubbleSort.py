745e56364665f42ad7a4cfdcade076784248149b
dtuyenng <136381756+dtuyenng@users.noreply.github.com>
2024-10-30 13:21:50 -0700

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,13 +1,27 @@
-arr = [5, 6, 3, 1, 9, 6, 7, 0]
+
+
+arr = [4, 2, 6, 5, 1, 3]
 
 def bubble_sort(array):
-    for _ in range(len(array)):
-        for index in range(len(array) - 1):
+    for i in range(len(array)):
+        for index in range(len(array) - 1 - i):
             curr  = array [index]
             after = array[index + 1]
             if curr > after:
                 array[index], array[index+1] = array[index+1], array[index]
 
+# arr = [4, 2, 6, 5, 1, 3]
+def selection_sort(array):
+    for i in range(len(array)):
+        min_index = i  # Start by assuming the minimum is the current position
+        for j in range(i + 1, len(array)):
+            if array[j] < array[min_index]:
+                min_index = j  # Update min_index if a smaller element is found
+        print("Current state of array:", array)
+        # Swap only if the minimum index has changed
+        if i != min_index:
+            array[i], array[min_index] = array[min_index], array[i]
 
-bubble_sort(arr)
+
+selection_sort(arr)
 print(arr)
