427bc7ade3d1fbd1038e3bb1a3aa87ae4aaa5cd9
dtuyenng <136381756+dtuyenng@users.noreply.github.com>
2024-11-24 20:03:56 -0800

    Update Task.py

--- Task.py
+++ Task.py
@@ -2,12 +2,44 @@
 import heapq
 
 class Task:
-    def __init__(self, name, priority):
-        self.name = name
+    def __init__(self, task_id, task_name, priority):
+        self.id = task_id
+        self.name = task_name
         self.priority = priority
 
-    def __lt(self, other):
-        if self.priority < other.priority:
-            return True
-    
+    def __lt__(self, other):
+        return self.priority < other.priority
 
+
+    def __gt__(self, other):
+        return self.priority > other.priority
+
+    def __eq__(self, other):
+        return self.priority == other.priority
+
+
+class TaskList:
+    def __init__(self):
+        self.id_counter = 1
+        self.priority_list = []
+
+    def add_task(self, task_name, priority):
+        new_task = Task(self.id_counter, task_name, priority)
+        # self.priority_list.insert(0, new_task)
+        heapq.heappush(self.priority_list, new_task)
+        self.id_counter += 1
+
+    def print(self):
+        for item in self.priority_list:
+            print(item.id, item.name, item.priority)
+
+
+todo = TaskList()
+todo.add_task("Buy Milk", 61)
+todo.add_task("Do Homework", 1)
+todo.add_task("Wash Dishes", 2)
+
+# print(todo.priority_list[1].priority)
+# print(todo.priority_list[0].priority)
+# print(todo.priority_list[1] < todo.priority_list[0])
+todo.print()
